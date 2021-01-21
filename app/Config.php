43a316212d4ff73939045e77c9d76a8f39a98c2a
Alan Santos <alanssantos32@gmail.com>
2021-01-20 23:01:44 -0300

    new pdo

--- app/Config.php
+++ app/Config.php
@@ -2,4 +2,3 @@
 
 define("URL_BASE", "http://localhost:2000/Book-Point");
 // define("URL_DATABASE", "pgsql:host=ec2-54-225-18-166.compute-1.amazonaws.com;port=5432;dbname=deo974lsqfa7dd", "ldjshstlbhbchp", "7efbf1b9971a766813451c906b0663a86093fa8bb54c3788bcfb61ff8c9dd11b");
-define("URL_DATABASE", "postgres://ldjshstlbhbchp:7efbf1b9971a766813451c906b0663a86093fa8bb54c3788bcfb61ff8c9dd11b@ec2-54-225-18-166.compute-1.amazonaws.com:5432/deo974lsqfa7dd");
--- app/Models/Model.php
+++ app/Models/Model.php
@@ -2,15 +2,34 @@
 
 namespace MyApp\Models;
 
+use Csanquer\Silex\PdoServiceProvider\Provider\PdoServiceProvider;
+use Silex\Application;
+
 class Model {
 
     protected function connect() {
         // foreach(\PDO::getAvailableDrivers() as $driver) {
         //    echo $driver;
         // }
-        $db = new \PDO(URL_DATABASE);
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // $db = new \PDO(URL_DATABASE);
+        // $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         
-        return $db;
+        // return $db;
+        $dbopts = parse_url(getenv('DATABASE_URL'));
+        $app = new Application();
+        $app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
+            array(
+                'pdo.server' => array(
+                'driver'   => 'pgsql',
+                'user' => $dbopts["user"],
+                'password' => $dbopts["pass"],
+                'host' => $dbopts["host"],
+                'port' => $dbopts["port"],
+                'dbname' => ltrim($dbopts["path"],'/')
+                )
+            )
+        );
+
+        return $app['pdo'];
     }
 }
